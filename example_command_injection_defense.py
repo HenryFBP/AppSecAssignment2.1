55e442600251c8b96f1a179dcae53b62f2dc639f
Henry Post <HenryFBP@gmail.com>
2020-12-15 12:41:29 -0600

    rename CI example file

--- example_command_injection_defense.py
+++ example_command_injection_defense.py
@@ -2,7 +2,7 @@ import shlex, subprocess
 
 evil_data='; wall "You have been hacked!"'
 
-p = subprocess.Popen('ls', '-l', 'images/', evil_data)
+p = subprocess.Popen('ls', '-l', 'images/', evil_data) # popen defends against this
 
 p.wait()
 
